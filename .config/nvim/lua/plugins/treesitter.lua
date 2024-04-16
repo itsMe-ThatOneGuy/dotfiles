28ac2448f535dab8eee25b4cabfd7adeec905eda
Matthew Smart <matthewsmart13@gmail.com>
2024-04-16 13:11:57 -0500

    Added treesitter-context keymap

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -38,6 +38,10 @@ return {
 					},
 				},
 			})
+
+			vim.keymap.set("n", "[c", function()
+				require("treesitter-context").go_to_context(vim.v.count1)
+			end, { silent = true })
 		end,
 	},
 }
