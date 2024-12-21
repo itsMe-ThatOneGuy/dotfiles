ca7d21538a87d72d8378846aaaefb693bb1e4eba
Matthew Smart <matthewsmart13@gmail.com>
2024-12-21 10:16:13 -0600

    small term win map

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -78,3 +78,11 @@ keymap("n", "<S-q>", "<cmd>bdelete!<CR>", opts)
 
 -- Lsp
 keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", opts)
+
+-- Term
+keymap("n", "<leader>st", function()
+	vim.cmd.vnew()
+	vim.cmd.term()
+	vim.cmd.wincmd("J")
+	vim.api.nvim_win_set_height(0, 5)
+end)
