b4ff6e56019b8d014823921ecf382bc3ac8159a8
Matthew Smart <matthewsmart13@gmail.com>
2025-05-27 13:44:42 -0500

    updated format

--- .config/nvim/lua/plugins/format_lint.lua
+++ .config/nvim/lua/plugins/format_lint.lua
@@ -15,13 +15,25 @@ return {
 				prettier = {
 					prepend_args = { "--single-quote", "--tab-width 4", "--use-tabs" },
 				},
+				injected = {
+					options = {
+						ignore_errors = false,
+						lang_to_formatters = {
+							sql = { "sqls" },
+						},
+						lang_to_ext = {
+							sql = "sql",
+						},
+					},
+				},
 			},
 			formatters_by_ft = {
 				lua = { "stylua" },
-				javascript = { { "prettierd", "prettier" } },
-				javascriptreact = { { "prettierd", "prettier" } },
-				typescript = { { "prettierd", "prettier" } },
-				typescriptreact = { { "prettierd", "prettier" } },
+				javascript = { "prettierd", "prettier" },
+				javascriptreact = { "prettierd", "prettier" },
+				typescript = { "prettierd", "prettier" },
+				typescriptreact = { "prettierd", "prettier" },
+				sql = { "injected" },
 			},
 		},
 	},
@@ -32,10 +44,10 @@ return {
 			local lint = require("lint")
 
 			lint.linters_by_ft = {
-				javascript = { "eslint" },
-				javascriptreact = { "eslint" },
-				typescript = { "eslint" },
-				typescriptreact = { "eslint" },
+				javascript = { "eslint_d" },
+				javascriptreact = { "eslint_d" },
+				typescript = { "eslint_d" },
+				typescriptreact = { "eslint_d" },
 			}
 
 			vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePost" }, {
