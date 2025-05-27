201b3998a180b95510b81ca2eaa638665e3381d9
Matthew Smart <matthewsmart13@gmail.com>
2025-05-27 13:44:32 -0500

    removed font

--- .config/nvim/lua/set.lua
+++ .config/nvim/lua/set.lua
@@ -83,7 +83,6 @@ vim.opt.showcmd = false -- hide (partial) command in the last line of the screen
 vim.opt.ruler = false -- hide the line and column number of the cursor position
 vim.opt.numberwidth = 4 -- minimal number of columns to use for the line number {default 4}
 vim.opt.sidescrolloff = 8 -- minimal number of screen columns to keep to the left and right of the cursor if wrap is `false`
-vim.opt.guifont = "monospace:h17" -- the font used in graphical neovim applications
 vim.opt.fillchars.eob = " " -- show empty lines at the end of a buffer as ` ` {default `~`}
 vim.opt.shortmess:append("c") -- hide all the completion messages, e.g. "-- XXX completion (YYY)", "match 1 of 2", "The only match", "Pattern not found"
 vim.opt.whichwrap:append("<,>,[,],h,l") -- keys allowed to move to the previous/next line when the beginning/end of line is reached
