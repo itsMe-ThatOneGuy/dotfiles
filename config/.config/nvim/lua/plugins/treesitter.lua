caf920c085c800f95b5cf885ab49629ca8f2b2ab
itsMe-ThatOneGuy <matthewsmart13@gmail.com>
2026-08-14 13:50:38 -0500

    Migrated to nvim .12 update

--- config/.config/nvim/lua/plugins/treesitter.lua
+++ config/.config/nvim/lua/plugins/treesitter.lua
@@ -1,47 +1,16 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		branch = "main",
+		build = ":TSUpdate",
+
 		dependencies = {
 			"windwp/nvim-ts-autotag",
-			"nvim-treesitter/nvim-treesitter-context",
 		},
-		build = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-					"javascript",
-					"json",
-					"c",
-					"lua",
-					"vim",
-					"vimdoc",
-					"query",
-					"html",
-					"css",
-				},
-				sync_install = false,
-				modules = {},
-				ignore_install = {},
-				auto_install = true,
-				indent = { enable = true },
-				highlight = { enable = true },
-				autotag = {
-					enable = true,
-				},
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "<CR>",
-						node_incremental = "<TAB>",
-						scope_incremental = "<CR>",
-						node_decremental = "<S-TAB>",
-					},
-				},
-			})
 
-			vim.keymap.set("n", "[c", function()
-				require("treesitter-context").go_to_context(vim.v.count1)
-			end, { silent = true })
+		config = function()
+			require("nvim-treesitter").setup()
 		end,
 	},
 }
